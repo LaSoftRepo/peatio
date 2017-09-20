09f8cf52b4ade9a930f27aec4af53b7307c4ef8a
Vitaly Paprotsky <vetalpaprotsky@gmail.com>
2017-09-20 20:50:16 +0300

    add some docker configurations

---
+++ docker/bin/base.sh
@@ -0,0 +1,3 @@
+docker run -d --name redis redis:3.2
+docker run -d --name dbmaster -v /docker_volumes/dbmysql:/var/lib/mysql -e MYSQL_ROOT_PASSWORD=123456 -e MYSQL_USER=my_user -e MYSQL_PASSWORD=123456 -e MYSQL_DATABASE=my_database mysql:5.7
+docker run -d --name rabbitmq rabbitmq:3.6
